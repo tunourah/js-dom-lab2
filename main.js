77045e90f0ce608e38060c8ae090577c935757ee
noura <noura.tu19@gmail.com>
2024-09-11 14:00:17 +0300

    edit the width and height

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ setInterval(() => {
     squerEle.style.left = Math.floor(Math.random() * 100) + 'px';
     squerEle.style.top = Math.floor(Math.random() * 100) + 'px';
     squerEle.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    squerEle.style.width = Math.floor(Math.random() * 100) + 'px';
-    squerEle.style.height = Math.floor(Math.random() * 100) + 'px';
+    squerEle.style.width = Math.floor(Math.random() * 200) + 'px';
+    squerEle.style.height = Math.floor(Math.random() *200) + 'px';
     
 
 }
