5643d2309f1c2190139c1d7d1ac670c254b7229d
noura <noura.tu19@gmail.com>
2024-09-11 14:28:41 +0300

    play with the code

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-squerEle = document.getElementById('squer');
+const squerEles = [
+    document.getElementById('squer'),
+    document.getElementById('squer1'),
+    document.getElementById('squer2'),
+    document.getElementById('squer3')
+];
 
 
-setInterval(() => {
+function squerMove(squerEle) {
     squerEle.style.left = Math.floor(Math.random() * 100) + 'px';
     squerEle.style.right = Math.floor(Math.random() * 100) + 'px';
     squerEle.style.top = Math.floor(Math.random() * 100) + 'px';
@@ -9,8 +14,12 @@ setInterval(() => {
     squerEle.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
     squerEle.style.width = Math.floor(Math.random() * 200) + 'px';
     squerEle.style.height = Math.floor(Math.random() *200) + 'px';
-    
-
+    squerEle.style.transformY = Math.floor(Math.random() * -100) + 'px';
+    squerEle.style.transformX = Math.floor(Math.random() * 100) + 'px';}
+setInterval(() => {
+    squerEles.forEach(squerMove);
 }
 
 , 2000);
+
+// make it more elegant ☺️
