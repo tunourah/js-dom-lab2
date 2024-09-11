c41e6acfa88a782d7033f9f9b3f196b2c7f5f065
noura <noura.tu19@gmail.com>
2024-09-11 14:33:29 +0300

    play with code

--- main.js
+++ main.js
@@ -7,10 +7,7 @@ const squerEles = [
 
 
 function squerMove(squerEle) {
-    squerEle.style.left = Math.floor(Math.random() * 100) + 'px';
-    squerEle.style.right = Math.floor(Math.random() * 100) + 'px';
-    squerEle.style.top = Math.floor(Math.random() * 100) + 'px';
-    squerEle.style.bottom = Math.floor(Math.random() * 100) + 'px';
+   
     squerEle.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
     squerEle.style.width = Math.floor(Math.random() * 200) + 'px';
     squerEle.style.height = Math.floor(Math.random() *200) + 'px';
